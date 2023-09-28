f89db6b85528b51c56ac4465c6b0695e92a5be2c
emil476m <emilwiltmann.dk@gmail.com>
2023-09-28 11:45:59 +0200

    made corrections to spelling mistakes in reposetory

--- Infarstructure/Repository.cs
+++ Infarstructure/Repository.cs
@@ -12,19 +12,24 @@ public class Repository
         _dataSource = dataSource;
     }
     
-    public Box CreateBox(string name, string size, string description, float price, string articleImgUrl)
+    public Box CreateBox(string name, string size, string description, float price, string boxImgUrl)
     {
-        var sql = $@"INSERT INTO box (name, size, description, price, articleimgurl) VALUES(@name, @size, @description,@price, @articleImgUrl) RETURNING 
+        var sql = $@"INSERT INTO box (name, size, description, price, boxImgUrl) VALUES(@name, @size, @description,@price, @boxImgUrl) RETURNING 
         boxid as {nameof(Box.boxId)}, 
         name as {nameof(Box.name)},
         size as {nameof(Box.size)}, 
         description as {nameof(Box.description)}, 
         price as {nameof(Box.price)}, 
-        articleimgurl as {nameof(Box.boxImgUrl)};";
+        boximgurl as {nameof(Box.boxImgUrl)};";
         
         using (var conn = _dataSource.OpenConnection())
         {
-            return conn.QueryFirst<Box>(sql, new {name, size, description, price, articleImgUrl});
+            return conn.QueryFirst<Box>(sql, new {name, size, description, price, boxImgUrl});
         }
+    }
+
+    public void DeleteBox(int boxId)
+    {
+        
     }
 }
