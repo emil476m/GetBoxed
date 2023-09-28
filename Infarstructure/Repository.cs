28fc281e1e5d783f6b3910ea61b65390a0b6eae6
emil476m <emilwiltmann.dk@gmail.com>
2023-09-28 10:35:04 +0200

    set up basics for Controller, Services and reposetory

---
+++ Infarstructure/Repository.cs
@@ -0,0 +1,13 @@
+﻿using Npgsql;
+
+namespace Infarstructure;
+
+public class Repository
+{
+    private readonly NpgsqlDataSource _dataSource;
+
+    public Repository(NpgsqlDataSource dataSource)
+    {
+        _dataSource = dataSource;
+    }
+}
--- Services/Service.cs
+++ Services/Service.cs
@@ -1,5 +1,13 @@
-﻿namespace Service;
+﻿using Infarstructure;
+
+namespace Service;
 
 public class Service
 {
+    private readonly Repository _repository;
+
+    public Service(Repository repository)
+    {
+        _repository = repository;
+    }
 }
