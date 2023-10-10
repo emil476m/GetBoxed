398075deea732983d480f6af8253905a8f1f7b31
TheDarkSide1992 <Jenscp171@gmail.com>
2023-10-10 14:36:22 +0200

    api part of data for graphs are finished

--- frontend/src/app/GraphModel/graphModels.ts
+++ frontend/src/app/GraphModel/graphModels.ts
@@ -23,21 +23,45 @@ import {HttpClient} from "@angular/common/http";
     `,
 })
 
-export class graphModels implements OnInit
-{
-
-constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
+export class graphModels implements OnInit {
+  constructor(private http: HttpClient) {
   }
-  chartData = [ //This is mock data and should be replaced
+  chartData:any[] = [] /*[
+    //This is mock data and should be replaced
     {data: [65, 59, 80, 81, 56, 55, 40, 12, 87, 40, 50, 99], label: 'Box A'},
     {data: [65 ,0, 0, 3, 2, 86,1 ,79, 9, 12, 38, 64], label: 'Box B'},
     {data: [2 ,76, 34, 2, 96, 2, 3, 4, 100, 45, 28, 66], label: 'Box c'},
-  ];
+  ];*/
   chartLabels = ['1. January', '2. February', '3. March', '4. April', '5. May', '6. June', '7. July', '8. August', '9. September', '10. October', '11. November', '12. December'];
   chartOptions = {
     responsive: true
   };
   chartType = 'bar';
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  async getData() {
+    const result = this.http.get<boxdata>("http://localhost:5000/GraphData/");
+
+    result.subscribe((resData) =>{
+      let dataset = {
+        label: resData.boxname,
+        data: resData.soldPrMonth
+      };
+      this.chartData.push(dataset)
+    })
+
+  }
+
+
 }
+
+export interface boxdata{
+  soldPrMonth : number[]
+  boxname : string
+  boxid : number
+}
+
+
